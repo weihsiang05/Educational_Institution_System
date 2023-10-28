5c9b9a516ec9d8870495f1b9f51807cd412cf663
weihsiang05 <weihsiang0000@gmail.ocm>
2023-10-28 01:32:12 -0400

    Router init

--- app.js
+++ app.js
@@ -10,6 +10,42 @@ app.get('/cramSchool', (req, res) => {
   res.send('Hello!')
 })
 
+//Render new student page
+app.get('/crameSchool/new/student', (req, res) => {
+
+})
+
+//Create new student
+app.post('/crameSchool/new/student', (req, res) => {
+
+})
+
+//render new subject page
+app.get('/crameSchool/new/subject', (req, res) => {
+
+})
+
+//Create new subject
+app.post('/crameSchool/new/subject', (req, res) => {
+
+})
+
+app.get('/crameSchool/:studentID/edit/homework', (req, res) => {
+
+})
+
+app.post('/crameSchool/:studentID/edit/homework/add', (req, res) => {
+
+})
+
+app.put('/crameSchool/:studentID', (req, res) => {
+
+})
+
+app.delete('/crameSchool/:studentID', (req, res) => {
+
+})
+
 app.listen(port, () => {
   console.log(`express server is running on the http://localhost:${port}`)
 })
