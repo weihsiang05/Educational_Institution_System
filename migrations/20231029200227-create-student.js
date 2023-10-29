85667072fb8a474a4439e273ab6fbdac7d2cfbf1
weihsiang05 <weihsiang0000@gmail.ocm>
2023-10-29 16:12:48 -0400

    install & initialize sequelize

---
+++ migrations/20231029200227-create-student.js
@@ -0,0 +1,32 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('students', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING
+      },
+      FiristName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      LastName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('students');
+  }
+};
